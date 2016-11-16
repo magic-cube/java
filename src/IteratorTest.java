079088912acc05264b580f5bf7f61271e0c0dcf3
magic-cube <15619225472@163.com>
2016-11-16 17:58:43 +0800

    Iterator s removee

---
+++ src/IteratorTest.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+//Iterator对象的remove方法是迭代过程中删除元素的唯一的安全的方法
+public class IteratorTest {
+	
+	public static void main(String[] args) {
+		Collection<String > c = new ArrayList<String>();
+		c.add("aaa");
+		c.add("bbbb");
+		c.add("c");
+		Iterator i =c.iterator();
+		while(i.hasNext()){
+			String s=(String) i.next();
+			
+			if(s.length()>3){
+				i.remove();
+				//若果是c.remove()  会产生例外
+			}
+			System.out.println(s);
+			//System.out.println((String)i.next());
+		}
+	}
+
+}
