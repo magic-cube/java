9240ed13617366b395a1098974285bc828f26235
magic-cube <15619225472@163.com>
2016-12-19 17:49:44 +0800

    test

---
+++ src/java2/TCPClient.java
@@ -0,0 +1,21 @@
+package java2;
+import java.io.*;
+import java.net.*;
+
+public class TCPClient {
+	public static void main(String args[]){
+		//请求连接
+		try{
+			Socket client=new Socket("127.0.0.1",9999);
+			//利用流传送数据
+			DataOutputStream dos=new DataOutputStream(client.getOutputStream());
+			dos.writeInt(10);
+			
+			dos.close();
+			client.close();
+		}catch(IOException e){
+			System.out.println("请求建立连接失败");
+			e.printStackTrace();
+		}
+	}
+}
