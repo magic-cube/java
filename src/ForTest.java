75d7d5da7b279c26b710e5f45ad55a6370eace92
magic-cube <15619225472@163.com>
2017-01-15 22:54:46 +0800

    增强for

--- src/ForTest.java
+++ src/ForTest.java
@@ -1,5 +1,11 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -27,4 +33,73 @@ public class ForTest {
 			System.out.println(i);
 		}
 	}
+	
+	/*
+	 * map的遍历,第一种方式,得到keySet，再用map.get()得到值;
+	 */
+	@Test
+	public void test3(){
+		Map<String,String> map=new HashMap<String,String>();
+		map.put("1", "aaa");
+		map.put("2", "bbb");
+		map.put("3", "ccc");
+		Set<String> set=map.keySet();
+		Iterator<String> it=set.iterator();
+		while(it.hasNext()){
+			String key=it.next();
+			String value=map.get(key);
+			System.out.println(key+":"+value);
+		}
+	}
+	/*
+	 * map的遍历,第二种方式
+	 */
+	@Test
+	public void test4(){
+		Map<String,String> map=new HashMap<String,String>();
+		map.put("1", "aaa");
+		map.put("2", "bbb");
+		map.put("3", "ccc");
+		Set entryset=map.entrySet();
+		Iterator it=entryset.iterator();
+		while(it.hasNext()){
+			Map.Entry<String, String> entry=(Entry<String, String>) it.next();
+			String key=entry.getKey();
+			String value=entry.getValue();
+			System.out.println(key+" "+value);
+		}
+	}
+	
+	/*
+	 * 增强for遍历map的第一种方式
+	 */
+	@Test
+	public void test5(){
+		Map<String,String> map=new HashMap<String,String>();
+		map.put("1", "aaa");
+		map.put("2", "bbb");
+		map.put("3", "ccc");
+		for(String m:map.keySet()){
+			String key=m;
+			String value=map.get(key);
+			System.out.println(key+" "+value);
+		}
+	}
+	
+	/*
+	 * 增强for遍历map的第二种方式
+	 */
+	@Test
+	public void test6(){
+		Map<String,String> map=new HashMap<String,String>();
+		map.put("1", "aaa");
+		map.put("2", "bbb");
+		map.put("3", "ccc");
+		for(Entry<String, String> m:map.entrySet()){
+			String key=m.getKey();
+			String value=m.getValue();
+			System.out.println(key+" "+value);
+		}
+	}
+	
 }
