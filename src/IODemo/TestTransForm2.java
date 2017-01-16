0936d40b3b343431460edd4e0b4422caf15f7aab
magic-cube <15619225472@163.com>
2017-01-16 15:18:08 +0800

    转换流

--- src/IODemo/TestTransForm2.java
+++ src/IODemo/TestTransForm2.java
@@ -2,6 +2,7 @@ package IODemo;
 import java.io.*;
 /**
  * 转换流
+ * 将字节流转换成字符流
  * OutputStreamWriter
  * true,在原有基础上添加
  * @author hc
@@ -9,18 +10,19 @@ import java.io.*;
 public class TestTransForm2 {
 	public static void main(String args[]){
 		try{
-			OutputStreamWriter ops= new OutputStreamWriter(new FileOutputStream("C:/my/hc.txt"/*,true*/));
+			OutputStreamWriter ops= new OutputStreamWriter(new FileOutputStream("C:/my/hc.txt",true));
 			String s;
-			BufferedReader brd = new BufferedReader(new FileReader("C:/my/liuke.txt"));
+//			BufferedReader brd = new BufferedReader(new FileReader("C:/my/liuke.txt"));
 //			while((s=brd.readLine())!=null){
 //				ops.write(s);
 //				System.out.println();
 //			}
+			//可以直接写一个字符串进去
 			ops.write("abcdefj higklmn opq rst uvw xyzxyz now you see i can say my abc");
 			//将输出流中的数据写出到目的地
 			ops.flush();
 			//要记得关
-			brd.close();
+//			brd.close();
 			ops.close();
 		}catch(IOException e){
 			System.out.println("有猫饼！");
